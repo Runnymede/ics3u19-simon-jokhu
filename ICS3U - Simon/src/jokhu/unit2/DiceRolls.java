88c1e3b28f5c37da3a65cd7a4bb31ff2c3b4b0f9
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2019-11-21 13:55:43 -0500

    Updates

--- ICS3U - Simon/src/jokhu/unit2/DiceRolls.java
+++ ICS3U - Simon/src/jokhu/unit2/DiceRolls.java
@@ -19,11 +19,22 @@ public class DiceRolls {
 		int diceRollTwo=0;
 		int total=0;
 		int totalTwo=0;
+		int counterTwo=0;
 		//prints Dice table
 		System.out.println("Dice1 Dice2 Total");
 	//Dice roll gets a value of dice twice then prints the roll
-		for(int counter=0;counter>4;counter++) {
+		for(int counter=0;counter<10;counter++) {
 			diceRoll=(int)(Math.random()*6)+1;
+			diceRollTwo=diceRoll;
+			System.out.print(diceRoll+"      ");
+			counterTwo++;
+			if (counterTwo==2) {
+			System.out.print(diceRoll+diceRollTwo);
+			System.out.println();
+				counterTwo=0;
+				
+				
+			}
 		}
 		
 	}

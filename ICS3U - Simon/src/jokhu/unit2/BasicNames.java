63a8d56f0c24c499825662ef6001a58d0dd9a165
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2019-11-22 15:06:02 -0500

    Begining of names

---
+++ ICS3U - Simon/src/jokhu/unit2/BasicNames.java
@@ -0,0 +1,17 @@
+package jokhu.unit2;
+
+import java.util.Scanner;
+
+public class BasicNames {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("How many names you got?");
+		int namesamount=sc.nextInt();
+		
+System.out.println("What are your names?"); 
+		String [] names =sc.nextLine();
+	}
+
+}

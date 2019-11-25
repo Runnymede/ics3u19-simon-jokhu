154535019ea702a9ef3f50c2e1cd4f6bc5ff475f
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2019-11-25 13:44:46 -0500

    Basic names reverse order

--- ICS3U - Simon/src/jokhu/unit2/BasicNames.java
+++ ICS3U - Simon/src/jokhu/unit2/BasicNames.java
@@ -6,12 +6,24 @@ public class BasicNames {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("How many names you got?");
-		int namesamount=sc.nextInt();
+		int namesAmount=sc.nextInt();
+String[]names1 =new  String[namesAmount];
 		
 System.out.println("What are your names?"); 
-		String [] names =sc.nextLine();
+		String names= sc.nextLine();
+		//names1.length()
+	
+		for(int counter=0;counter<namesAmount;counter++) {
+			names1[counter]=sc.next();
+			System.out.println(names1[counter]);
+		}
+		//reverse Order in progress
+		for(int counter=0;counter<namesAmount;counter++) {
+			names1[counter]=sc.next();
+			System.out.println(names1[counter]);
+		}
 	}
 
 }

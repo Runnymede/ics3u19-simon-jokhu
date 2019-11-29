63c6d19f2e1b9ad6b530e37360fd4a5688083a9c
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2019-11-29 13:28:42 -0500

    Tic-Tac-Toe COMPLETED!

--- ICS3U - Simon/src/jokhu/unit2/TicTacToe.java
+++ ICS3U - Simon/src/jokhu/unit2/TicTacToe.java
@@ -7,7 +7,7 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Lets play tic-tac-toe!");
-		
+
 		//player one loop
 		int end=1;
 		String again ="";
@@ -16,12 +16,25 @@ public class TicTacToe {
 			char [][] board=new char[3][3];
 			for (int counter=0;counter<4;counter++) {
 				if (end!=1) break;
+				//Ask player for where they would like to go
 				System.out.println("What row would you like to go in starting from 0, player 1");
-				int playerOneR=sc.nextInt();
+				int playerOneR=sc.nextInt()-1;
 				System.out.println("What coloum would you like to go in starting from 0, player 1");
-				int playerOneC=sc.nextInt();
-				board[playerOneR][playerOneC]='x';
-
+				int playerOneC=sc.nextInt()-1;
+				//Checks to see if a spot has been taken
+				do {
+				if (board[playerOneR][playerOneC]!='x' && board[playerOneR][playerOneC]!='o'){
+					board[playerOneR][playerOneC]='x';
+					break;
+				}
+				else {
+					System.out.println("Already Taken! What row would you like to go in starting from 0, player 1");
+					playerOneR=sc.nextInt()-1;
+					System.out.println("What coloum would you like to go in starting from 0, player 1");
+					playerOneC=sc.nextInt()-1;
+				}
+			}while (board[playerOneR][playerOneC]!=' ');
+				// Checks to see if player 1 has gotten a 3 in a row it win
 				if (board[0][0]=='x' && board[0][1]=='x' && board[0][2]=='x') {
 					System.out.println("Player 1 wins");
 					end++;
@@ -67,11 +80,20 @@ public class TicTacToe {
 					if (end!=1) break;
 					counter=0;
 					System.out.println("What row would you like to go in starting from 0, player 2");
-					int playerTwoR=sc.nextInt();
+					int playerTwoR=sc.nextInt()-1;
 					System.out.println("What coloum would you like to go in starting from 0, player 2");
-					int playerTwoC=sc.nextInt();
-					board[playerTwoR][playerTwoC]='o';
-
+					int playerTwoC=sc.nextInt()-1;
+					//Checks to see if a spot is already taken.
+					if (board[playerTwoR][playerTwoC]!='x' && board[playerTwoR][playerTwoC]!='o'){
+						board[playerTwoR][playerTwoC]='o';
+					}
+					else {
+						System.out.println("Already Taken! What row would you like to go in starting from 0, player 2");
+						playerTwoR=sc.nextInt()-1;
+						System.out.println("What coloum would you like to go in starting from 0, player 2");
+						playerTwoC=sc.nextInt()-1;
+					}
+//Checks to see if player two has a 3 and a row to win
 					if (board[0][0]=='o' && board[0][1]=='o' && board[0][2]=='o') {
 						System.out.println("Player 2 wins");
 						end++;
@@ -105,6 +127,7 @@ public class TicTacToe {
 						end++;
 					}
 					if (end!=1) break;
+					//Prints Out tic tac toe board
 					for (int row=0;row<board.length;row++) {
 						for (int col=0;col<board[0].length;col++) {
 							System.out.print("|"+board[row][col]+"|");

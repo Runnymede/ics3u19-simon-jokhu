3f777d6bcb2acdfe4e725ea21f6762ee239faaf1
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2019-12-19 15:14:36 -0500

    Triangle program done need comments

--- ICS3U - Simon/src/jokhu/unit4/IsoTriangles.java
+++ ICS3U - Simon/src/jokhu/unit4/IsoTriangles.java
@@ -4,41 +4,44 @@ import java.util.Scanner;
 
 public class IsoTriangles {
 
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Give me a number to make a triangle.");
 		int tri = sc.nextInt();
+//spaces tri-i
+//stars i+i-1
 		
-		for(int end=0;end<tri;end++){
+		for(int i=1;i<=tri;i++) {
 			
-			drawSpaces(tri);
-			drawStars(tri);
 			
+			drawSpaces(tri-i);
 		
-			spaces--;
-			stars+=2;
-			System.out.println();
-		}
+			drawStars(i+i-1);
+		System.out.println();
+	}
+			
 
+
+
+		
+
+	
 	}
 
-	public static void drawSpaces(int x) {
-		int spaces=x-1;
-		int counter=spaces;
-			
-		while(spaces>0) {
-			System.out.print(" ");
-			spaces--;
-		}
 
+	public static void drawSpaces(int x) {
+		for(int counter=0;counter<x;counter++) {
+			System.out.print(" ");
+
+		}
 
 	}
 	public static void drawStars(int y) {
-	int rows=0;
 		for(int counter=0;counter<y;counter++) {
 			System.out.print("*");
-			if(rows<y)
-				System.out.println();
 		}
+	
 	}
 }
+

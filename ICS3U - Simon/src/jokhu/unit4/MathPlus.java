235cb1642c92478df30ed12e585fda3c33f292ad
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2020-01-06 15:07:15 -0500

    Math plus

---
+++ ICS3U - Simon/src/jokhu/unit4/MathPlus.java
@@ -0,0 +1,21 @@
+package jokhu.unit4;
+
+public class MathPlus {
+
+	public static void main(String[] args) {
+	int array[] = {5,10,6,2,3,4,8};
+
+	}
+
+	public static int min(int[]minArray,int a) {
+		for(int lessThan=0;lessThan<minArray.length;lessThan++) {
+			minArray[lessThan];
+		}
+		
+		
+		
+		
+		return a;
+		
+	}
+}

22a07c0c4cbc6e7499b831a6f4dc682d63084700
333702769 <333702769@C-DS23WR1.tdsb.on.ca>
2020-01-15 13:44:32 -0500

    math class content done

--- ICS3U - Simon/src/jokhu/unit4/TechQuiz.java
+++ ICS3U - Simon/src/jokhu/unit4/TechQuiz.java
@@ -149,5 +149,170 @@ public class TechQuiz {
 		answer[6]=3;
 		answer[7]=2;
 		answer[8]=4;
+		return answer;
+		return choices;
+		return quiz;
+	}
+	public static String mathClassQuiz(String []quiz,int[]answer,String [][]choices) {
+		quiz[0] = "1.What does math.abs do?";
+		quiz[1] = "2.What does math.cbrt do";
+		quiz[2] = "3.what does math.rint do?";
+		quiz[3] = "4.What does a switch statement test?";
+		quiz[4] = "5.What does the following statement print?\n int month=8; \n switch(month) \n {\n case 1: system.out.println(\"January\");\n case 2: system.out.println(\"Febuary\");\n case 3: system.out.println(\"March\");\n case 4: system.out.println(\"April\");\n case 5: system.out.println(\"May\");\n case 6: system.out.println(\"June\");\n case 7: system.out.println(\"July\");\n case 8: system.out.println(\"August\");\n case 9: system.out.println(\"Sepetember\");\n case 10: system.out.println(\"October\");\n case 11: system.out.println(\"November\");\n case 12: system.out.println(\"December\");\n default: break;\n}";
+		quiz[5] = "6.When does a switch statement end?";
+		quiz[6] = "7.What does a.substring(3,6) do?";
+		quiz[7] = "8.What does str.equals(y) do?";
+		quiz[8] = "9.what does z.trim do?";
+		quiz[9] = "10.What does word.split(\" \") do?";
+
+		choices[0][0]="1.It returns all positives to negatives and positives to negatives";	
+		choices[0][1]="2.It returns negatives to positives";
+		choices[0][2]="3.It returns positve to negatives";
+		//
+		choices[0][3]="4.It returns negative to positives and positives to positives";
+
+		//
+		choices[1][0]="1.It returns the cube root of a double value";
+		choices[1][1]="2.It returns the cube root of a int value";
+		choices[1][2]="3.It returns the square root of a double value";
+		choices[1][3]="4.It returns the square root of a int value";
+
+		choices[2][0]="1.It cuts of remaining decimals of a value";
+		//
+		choices[2][1]="2.It rounds remaining decimals of a value";
+		choices[2][2]="3.It gives the exponent of a value";
+		choices[2][3]="4.It turns a double value into an int value";
+
+		choices[3][0]="1.It test for any boolean expression";
+		choices[3][1]="2.It test for inequality";
+		choices[3][2]="3.It test for only greater than and less than";
+		//
+		choices[3][3]="4.It test for equality";
+		
+		choices[4][0]="1.January-August";
+		//
+		choices[4][1]="2.August-December";
+		choices[4][2]="3.August";
+		choices[4][3]="4.January-December";
+
+		choices[5][0]="1.When it goes through all the code";
+	//
+		choices[5][1]="2.When it reaches a break statement";
+		choices[5][2]="3.It doesn't end";
+		choices[5][3]="4.When It reaches an invalid statement";
+		
+		choices[6][0]="1.Returns the portion of string a from 3 to 6";
+		//
+		choices[6][1]="2.Returns the portion of string a from 3 to 5";
+		choices[6][2]="3.Returns the portion of string a at point 3 and point 6";
+		choices[6][3]="4.Returns the portion of string a at point 3 and point 5";
+		
+		choices[7][0]="1.Returns false if the contents of string is exatly the same as the contens of string y";
+		choices[7][1]="2.Returns true if the contents of string is almost exatly the same as the contens of string y";
+		choices[7][2]="3.Returns true if the contents of string is not exatly the same as the contens of string y";
+		//
+		choices[7][3]="4.Returns true if the contents of string is exatly the same as the contens of string y";
+		
+		//
+		choices[8][0]="1.Returns a new string that is the same as z except all whitespace characters(spaces,tabs, and carriage returns) have been removed from beginning to end";
+		choices[8][1]="2.Returns a new string that is the same as z except the last half of the string has been removed";
+		choices[8][2]="3.Returns a new string that is the same as z except the first half of the string has been removed";
+		choices[8][3]="4.Returns a new string that is the same as z except all whitespace characters(spaces,tabs, and carriage returns) have not been removed from beginning to end";
+		
+		choices[9][0]="1.Splits a string at every character";
+		choices[9][1]="2.Splits a string in half";
+		choices[9][2]="3.Splits a string at every space";
+		choices[9][3]="4.Splits a string at every white space character";
+		
+		answer[0]=4;
+		answer[1]=1;
+		answer[2]=2;
+		answer[3]=4;
+		answer[4]=2;
+		answer[5]=2;
+		answer[6]=2;
+		answer[7]=4;
+		answer[8]=1;
+		answer[9]=3;
+		return answer;
+		return choices;
+		return quiz;
+	}
+	public static String booleanExpressions(String []quiz,int[]answer,String [][]choices) {
+		quiz[0] = "1.What does || operator mean?";
+		quiz[1] = "2.What does && operator mean?";
+		quiz[2] = "3.What does ! operator do?";
+		quiz[3] = "4.What is the correct order of operators?";
+		quiz[4] = "5.The \"less than or equal to\" comparison operator in java is_____.";
+		quiz[5] = "6.The equal comparison operator in java is_______.";
+		quiz[6] = "7.";
+		quiz[7] = "8.";
+		quiz[8] = "9.";
+		quiz[9] = "10.Which of the following is the correct expression that evaluates to true if the number x is between 1 and 100 or the number is negative?";
+
+		choices[0][0]="1.";	
+		choices[0][1]="2.";
+		choices[0][2]="3.";
+		choices[0][3]="4.";
+
+		choices[1][0]="1.";
+		choices[1][1]="2.";
+		choices[1][2]="3.";
+		choices[1][3]="4.";
+
+		choices[2][0]="1.";
+		choices[2][1]="2.";
+		choices[2][2]="3.";
+		choices[2][3]="4.";
+
+		choices[3][0]="1.";
+		choices[3][1]="2.";
+		choices[3][2]="3.";
+		choices[3][3]="4.";
+		
+		choices[4][0]="1.";
+		choices[4][1]="2.";
+		choices[4][2]="3.";
+		choices[4][3]="4.";
+
+		choices[5][0]="1.";
+		choices[5][1]="2.";
+		choices[5][2]="3.";
+		choices[5][3]="4.";
+		
+		choices[6][0]="1.";
+		choices[6][1]="2.";
+		choices[6][2]="3.";
+		choices[6][3]="4.";
+		
+		choices[7][0]="1.";
+		choices[7][1]="2.";
+		choices[7][2]="3.";
+		choices[7][3]="4.";
+		
+		//
+		choices[8][0]="1.";
+		choices[8][1]="2.";
+		choices[8][2]="3.";
+		choices[8][3]="4.";
+		
+		choices[9][0]="1.";
+		choices[9][1]="2.";
+		choices[9][2]="3.";
+		choices[9][3]="4.";
+		
+		answer[0]=4;
+		answer[1]=1;
+		answer[2]=2;
+		answer[3]=4;
+		answer[4]=2;
+		answer[5]=2;
+		answer[6]=2;
+		answer[7]=4;
+		answer[8]=1;
+		answer[9]=3;
+		return answer;
+		return choices;
+		return quiz;
 	}
 }
